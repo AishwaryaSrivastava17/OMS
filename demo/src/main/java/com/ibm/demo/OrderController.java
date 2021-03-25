34a2226a04e65d93bb4ad4c6beb45206df00bfa2
Aishwarya Srivastava <aishwaryaaaaaaaaaaa@gmail.com>
2021-03-25 12:49:35 +0530

    add service layer and validation

--- demo/src/main/java/com/ibm/demo/OrderController.java
+++ demo/src/main/java/com/ibm/demo/OrderController.java
@@ -1,18 +1,32 @@
 package com.ibm.demo;
 
-import org.springframework.http.HttpRequest;
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ibm.demo.entity.Order;
+
 @RestController
-public class OrderController {
+public class OrderController {//frontend
+	@Autowired//is used for DI
+	OrderService orderService;   //DI
 @PostMapping("/order")
-String createOrder() {
-	return "success";
+String createOrder(@RequestBody @Valid Order order,BindingResult bindingResult){//,Order price,Order name) {
+//String createOrder(@Valid Order order,BindingResult bindingResult) {	
+if(bindingResult.hasErrors()) {
+	throw new IllegalArgumentException("Something went wrong");
+}
+
+	System.out.println(order);
+	return orderService.createOrder(order);
 }
 @GetMapping("/order")
 String getOrder() {
@@ -25,8 +39,8 @@ String updateOrder(@PathVariable("id")int orderId) {
 
 }
 @DeleteMapping("/order/{id}")
-String deleteOrder(@PathVariable("id")int orderId,HttpRequest httprequest) {
-System.out.println(httprequest.getHeaders());
+String deleteOrder(@PathVariable("id")int orderId) {
+//System.out.println(httprequest.getHeaders());
 	System.out.println(orderId);
 	return "order deleted";
 }
